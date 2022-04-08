e8802e31aeeb1bf5da9b5729dd3087cc129b632d
Okekeugo <chuboyokeke@yahoo.com>
2022-04-08 23:56:25 +0000

    Task 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always(Success/Correct)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always(Success/Correct)
+ */
+int main(void)
+
+{
+int n;
+char ch;
+
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+
+}
