b72c3581394c5b41b4c1b7170040905e32cd6a4d
Okekeugo <chuboyokeke@yahoo.com>
2022-04-08 20:09:53 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/Correction)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet(++)
+			{ putcgar(alphabet);
+			}
+
+		putchar('\n');
+	return (0);
+}
