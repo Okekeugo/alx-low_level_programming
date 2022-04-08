df086a244e628506259e3ba22ad8b00b2e5a6bf7
Okekeugo <chuboyokeke@yahoo.com>
2022-04-08 23:20:52 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always(Success/Correct)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 41 ; i <= 57; i++)
+{
+	putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+}
