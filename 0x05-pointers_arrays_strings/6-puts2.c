166ee364b38aee3691ff5c597be4b49b3311c191
Okekeugo <chuboyokeke@yahoo.com>
2022-04-20 19:29:51 +0000

    Task 6 update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  *puts2 - prints every character of a string
- *   *@str: A pointer to an int that will be changed
- *    *
- *     *Return: void which means it is correct
- *      */
+ *puts2 - prints every character of a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means it is correct
+ */
 
 void puts2(char *str)
 {
