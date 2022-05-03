8ba49266a4f680a2c644ed75be38e3ecfe4a7159
Okekeugo <58901551+Okekeugo@users.noreply.github.com>
2022-05-03 03:08:41 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - is a function that prints the name.
  *@argc: is an argument counter for char.
  *@argv: is an agument value for char.
  *Return: Returns always success.
- **/
+**/
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+int i = argc - argc;
 
-	return (0);
+printf("%s\n", argv[i]);
+
+return (0);
 }
